1fd7ce68f07af20b40b43b7bf0528c7f35984545
LevyNat <levynath5@gmail.com>
2024-08-04 12:54:59 -0700

    update class - write unit test part

---
+++ car_class.py
@@ -0,0 +1,28 @@
+class Vehicle:
+    """A simple vehicle class"""
+
+    def __init__(self, make: str, model: str, year: int):
+        self.make = make
+        self.model = model
+        self.year = year
+        self._started = False
+
+    def start(self):
+        print("Starting engine...")
+        self._started = True
+
+    def stop(self):
+        print("Stopping engine...")
+        self._started = False
+
+
+class Car(Vehicle):
+    def __init__(self, make, model, year, num_seats):
+        super().__init__(make, model, year)
+        self.num_seats = num_seats
+
+    def drive(self):
+        print(f'Driving my "{self.make} - {self.model}" on the road')
+
+    def __str__(self):
+        return f'"{self.make} - {self.model}" has {self.num_seats} seats'
