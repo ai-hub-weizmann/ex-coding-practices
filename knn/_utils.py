4a374ee3d5459e049599b59868c03dd63b7f54d7
LevyNat <levynath5@gmail.com>
2024-08-05 00:01:22 -0700

    update knn until predict method

--- knn/_utils.py
+++ knn/_utils.py
@@ -1,8 +1,13 @@
 from sklearn.metrics.pairwise import euclidean_distances
+from scipy.spatial.distance import cdist, pdist, squareform
+
+# def calc_distance(x0, X):
+#     return euclidean_distances(x0.reshape(1, -1), X)
 
 
-def calc_distance(x0, X):
-    return euclidean_distances(x0.reshape(1, -1), X)
+def calc_distance(X, metric="euclidean"):
+    # return cdist(X, X, metric=metric)
+    return squareform(pdist(X, metric=metric))
 
 
-# accuracy
+# score
--- knn/knn.py
+++ knn/knn.py
@@ -3,13 +3,43 @@ import numpy as np
 
 from ._utils import calc_distance
 
-class KNeighborsClassifier:
-    def __init__(self, n_neighbors=5):
-        self.n_neighbors = n_neighbors
 
-    # def fit(self, X, y):
-    #     self.X = X
-    #     self.y = y
+class KNeighborsClassifier:
+    def __init__(self, n_neighbors=5, metric="euclidean", p=2):
+        self.n_neighbors = n_neighbors
+        self.metric = metric
+        self.p = p
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+        self.classes_ = np.unique(self.y_train)
+        self.n_classes = len(np.unique(self.y_train))
+
+        self.distances = calc_distance(self.X_train)
+        self.neighbors = np.argsort(self.distances, axis=1)[:, : self.n_neighbors]
+        self.neighbors_labels = self.y_train[self.neighbors]
+        self.neighbors_distances = self.distances[self.neighbors]
+
+    def predict(self, X):
+        if X is self.X_train:
+            return self.y_train
+        else:
+            distances = calc_distance(X, self.X_train, metric=self.metric, p=self.p)
+            neighbors = np.argsort(distances, axis=1)[:, : self.n_neighbors]
+            neighbors_labels = self.y_train[neighbors]
+
+        predictions = np.array(
+            [
+                np.argmax(np.bincount(neighbor_labels))
+                for neighbor_labels in neighbors_labels
+            ]
+        )
+
+        return self.classes_[predictions]
+
+    def predict_proba(self, X):
+        pass
 
     # def predict(self, X):
     #     y_pred = []
@@ -30,3 +60,7 @@ class KNeighborsClassifier:
     #         proba = np.bincount(k_nearest_labels, minlength=self.n_classes) / self.n_neighbors
     #         y_pred.append(proba)
     #     return np.array(y_pred)
+
+    def score(self, X, y):
+        y_pred = self.predict(X)
+        return np.mean(y_pred == y)
